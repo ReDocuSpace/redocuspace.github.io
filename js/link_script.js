bd7569a3498668d91183f8540ad73f52e54b0ce9
ReDocu <30718570+TakeKieer@users.noreply.github.com>
2022-05-31 02:20:24 +0900

    homepage setting

---
+++ js/link_script.js
@@ -0,0 +1,17 @@
+var change_page_main_introduce = document.querySelector('#change_page_main_introduce');
+var change_page_main_portfolio = document.querySelector('#change_page_main_portfolio');
+var change_page_main_project = document.querySelector('#change_page_main_project');
+var change_page_main_document = document.querySelector('#change_page_main_document');
+var change_page_main_board = document.querySelector('#change_page_main_board');
+var change_pdf_Career_Description = document.querySelector('#change_pdf_Career_Description');
+var change_test = document.querySelector('#change_test');
+var change_test_sub = document.querySelector('#change_test_sub');
+
+
+change_page_main_introduce.innerHTML = "HELLOL";
+
+change_page_main_introduce.onclick = function(){ location.href = "index.html"; }
+change_page_main_portfolio.onclick = function(){ location.href = "main_portfolio.html"; }
+change_page_main_project.onclick = function(){ location.href = "main_project.html"; }
+change_page_main_document.onclick = function(){ location.href = "main_document.html"; }
+change_pdf_Career_Description.onclick = function(){ location.href = "pdf/CareerDescription.pdf"; }
